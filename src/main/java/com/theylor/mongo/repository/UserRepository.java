f2091494d70d0376cacb0bd8eca8c962e668a80a
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-12-09 19:28:44 -0300

    Usando o padrão DTO para retornar Users

--- src/main/java/com/theylor/mongo/repository/UserRepository.java
+++ src/main/java/com/theylor/mongo/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.theylor.mongo.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
 import com.theylor.mongo.domain.User;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
 }
--- src/main/java/com/theylor/mongo/resources/UserResource.java
+++ src/main/java/com/theylor/mongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.theylor.mongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.theylor.mongo.domain.User;
+import com.theylor.mongo.dto.UserDTO;
 import com.theylor.mongo.services.UserService;
 
 @RestController
@@ -19,8 +21,11 @@ public class UserResource {
 	private UserService userService;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() { // O retorno será uma Lista de UserDTO
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		
+		// Conversão dos objetos User contidos em list para objetos UserDTO.
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
