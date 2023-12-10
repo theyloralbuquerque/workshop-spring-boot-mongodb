01799c1f7699ab8fc1a5cf909bdc58d01a305b89
Theylor Albuquerque <theylor18.albuquerque@gmail.com>
2023-12-09 23:53:45 -0300

    obtendo um usuário por id e trantando possíveis exceções

--- src/main/java/com/theylor/mongo/services/UserService.java
+++ src/main/java/com/theylor/mongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.theylor.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.theylor.mongo.domain.User;
 import com.theylor.mongo.repository.UserRepository;
+import com.theylor.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,12 @@ public class UserService {
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		
+		// retorna o obj ou lança uma exceção que é uma ObjectNotFoundException.
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
 }
+
